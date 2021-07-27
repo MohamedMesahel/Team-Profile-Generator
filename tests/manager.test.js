406edcd2e5c009152d88612591860cd9a5a8a1d1
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-07-27 12:49:23 -0700

    manager test.js

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -3,13 +3,14 @@ const Manager = require('../lib/manager');
 
 // Create Manager object  
 test('creates an Manager object', () => {
-    const manager = new Manager('Zizo', 86, 'zoz_2015@yahoo.com', 'MohamedMesahel');
+    const manager = new Manager('Zizo', 86, 'zoz_2015@yahoo.com', 4);
+
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // Role getRole()
 test('gets role of employee', () => {
-    const manager = new Manager('Zizo', 86, 'zoz_2015@yahoo.com', 'MohamedMesahel');
+    const manager = new Manager('Zizo', 90, 'zoz_2015@yahoo.com');
 
     expect(manager.getRole()).toEqual("Manager");
 });
